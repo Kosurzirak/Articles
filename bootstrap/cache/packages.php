cabef23ecb45fbd82838f0a0d7f65e03e85cf335
Kosurzirak <wimmiehilbillie@gmail.com>
2026-04-03 10:32:25 +0200

    Add files via upload

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,55 @@
+<?php return array (
+  'alirezasedghi/laravel-image-faker' => 
+  array (
+    'aliases' => 
+    array (
+      'ImageFaker' => 'Alirezasedghi\\LaravelImageFaker\\Facades\\ImageFaker',
+    ),
+    'providers' => 
+    array (
+      0 => 'Alirezasedghi\\LaravelImageFaker\\ImageFakerServiceProvider',
+    ),
+  ),
+  'laravel/pail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Pail\\PailServiceProvider',
+    ),
+  ),
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+);
